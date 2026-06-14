74ebf2331da511bf33192f73accb8aa082284a33
Lennard Idone <lennard.idone@idslatam.com>
2026-06-14 16:06:41 -0500

    cambios

--- go-api/main.go
+++ go-api/main.go
@@ -4,11 +4,13 @@ import (
 	"go-api/internal/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	app := fiber.New()
+	app.Use(cors.New())
 	routes.Register(app)
 
 	err := app.Listen("0.0.0.0:8080")
